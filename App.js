97f6c2e2fd7d471608dae3fee90564c2c53dc7b3
Wishcle <cpmqaq@gmail.com>
2019-06-28 11:51:57 -0400

    testing views

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+class Board extends Component {
+  render() {
+    return (
+      <View>
+        Test123
+      </View>
+    );
+  }
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello world!</Text>
     </View>
   );
 }
@@ -12,8 +22,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202020',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
